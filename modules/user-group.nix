30459c62433bedabc4d838f7e345595e65a00cd2
i1i1 <vanyarybin1@live.ru>
2023-12-19 19:14:42 +0300

    Move openssh and packages to `user-group.nix` and force or default base configuration

--- modules/user-group.nix
+++ modules/user-group.nix
@@ -1,4 +1,8 @@
-let
+{
+  lib,
+  pkgs,
+  ...
+}: let
   username = "rk";
   # To generate a hashed password run `mkpasswd`.
   # this is the hash of the password "rk3588"
@@ -6,6 +10,36 @@ let
   # TODO replace this with your own public key!
   publickey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK3F3AH/vKnA2vxl72h67fcxhIK8l+7F/bdE1zmtwTVU ryan@romantic";
 in {
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git # used by nix flakes
+    curl
+
+    neofetch
+    lm_sensors # `sensors`
+    btop # monitor system resources
+
+    # Peripherals
+    mtdutils
+    i2c-tools
+    minicom
+  ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = lib.mkDefault true;
+    settings = {
+      X11Forwarding = lib.mkDefault true;
+      PasswordAuthentication = lib.mkDefault true;
+    };
+    openFirewall = lib.mkDefault true;
+  };
+
   # =========================================================================
   #      Users & Groups NixOS Configuration
   # =========================================================================
@@ -29,4 +63,12 @@ in {
     "${username}" = {};
     docker = {};
   };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
 }
