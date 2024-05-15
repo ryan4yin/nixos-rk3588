c4fef04d8c124146e6e99138283e0c57b2ad8e29
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2024-05-15 04:40:48 +0300

    Modularize mali firmware and update it to latest, fixing OpenCL on Jellyfin (#35)
    * Allow G610 firmware to be disabled or replaced
    
    * Update mali firmware to latest

--- pkgs/mali-firmware/default.nix
+++ pkgs/mali-firmware/default.nix
@@ -4,11 +4,11 @@
 }:
 stdenv.mkDerivation {
   pname = "mali-g610-firmware";
-  version = "unstable-2022-11-21";
+  version = "g21p0-01eac0";
 
   src = fetchurl {
-    url = "https://github.com/JeffyCN/rockchip_mirrors/raw/309268f7a34ca0bba0ab94a0b09feb0191c77fb8/firmware/g610/mali_csffw.bin";
-    hash = "sha256-rZFRGnf6EQr0aiopytniJVJNnPqf41vc6WYG01TmB94=";
+    url = "https://github.com/JeffyCN/mirrors/raw/e08ced3e0235b25a7ba2a3aeefd0e2fcbd434b68/firmware/g610/mali_csffw.bin";
+    hash = "sha256-jnyCGlXKHDRcx59hJDYW3SX8NbgfCQlG8wKIbWdxLfU=";
   };
 
   buildCommand = ''
