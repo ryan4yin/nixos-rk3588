f8a554f5660b5502aa17582cf6560b4677b49af1
phaitonican <cenk.uluisik@googlemail.com>
2024-10-07 14:31:17 +0200

    Add Orange Pi 5b support with u-boot (#40)

---
+++ modules/boards/orangepi5b.nix
@@ -0,0 +1,48 @@
+# =========================================================================
+#      Orange Pi 5b Specific Configuration
+# =========================================================================
+{
+  pkgs,
+  rk3588,
+  ...
+}: let
+  pkgsKernel = rk3588.pkgsKernel;
+in {
+  imports = [
+    ./base.nix
+  ];
+
+  boot = {
+    kernelPackages = pkgsKernel.linuxPackagesFor (pkgsKernel.callPackage ../../pkgs/kernel/vendor.nix {});
+
+    # kernelParams copy from Armbian's /boot/armbianEnv.txt & /boot/boot.cmd
+    kernelParams = [
+      "rootwait"
+
+      "earlycon" # enable early console, so we can see the boot messages via serial port / HDMI
+      "consoleblank=0" # disable console blanking(screen saver)
+      "console=ttyS2,1500000" # serial port
+      "console=tty1" # HDMI
+
+      # docker optimizations
+      "cgroup_enable=cpuset"
+      "cgroup_memory=1"
+      "cgroup_enable=memory"
+      "swapaccount=1"
+    ];
+  };
+
+  # add some missing deviceTree in armbian/linux-rockchip:
+  # orange pi 5b's deviceTree in armbian/linux-rockchip:
+  #    https://github.com/armbian/linux-rockchip/blob/rk-5.10-rkr4/arch/arm64/boot/dts/rockchip/rk3588s-orangepi-5b.dts
+  hardware = {
+    deviceTree = {
+      name = "rockchip/rk3588s-orangepi-5b.dtb";
+      overlays = [];
+    };
+
+    firmware = [
+      (pkgs.callPackage ../../pkgs/orangepi-firmware {})
+    ];
+  };
+}
