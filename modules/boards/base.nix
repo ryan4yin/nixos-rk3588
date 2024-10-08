2b24e69ac4f3f784b5a084be4bdf402da830b95a
Cenk Uluisik <cenk.uluisik@googlemail.com>
2024-10-08 19:51:57 +0200

    Remove mesa panfork override

--- modules/boards/base.nix
+++ modules/boards/base.nix
@@ -75,27 +75,6 @@
     };
 
     hardware = {
-      # driver & firmware for Mali-G610 GPU
-      # it works on all rk2588/rk3588s based SBCs.
-      opengl.package =
-        (
-          (pkgs.mesa.override {
-            galliumDrivers = ["panfrost" "swrast"];
-            vulkanDrivers = ["swrast"];
-          })
-          .overrideAttrs (_: {
-            pname = "mesa-panfork";
-            version = "23.0.0-panfork";
-            src = pkgs.fetchFromGitLab {
-              owner = "panfork";
-              repo = "mesa";
-              rev = "120202c675749c5ef81ae4c8cdc30019b4de08f4"; # branch: csf
-              hash = "sha256-4eZHMiYS+sRDHNBtLZTA8ELZnLns7yT3USU5YQswxQ0=";
-            };
-          })
-        )
-        .drivers;
-
       enableRedistributableFirmware = lib.mkForce true;
       firmware = lib.mkIf config.hardware.mali.enableFirmware [
         config.hardware.mali.firmwarePackage
