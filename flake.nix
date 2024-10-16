8f5afee341cd4c500729c1927dfffa4e8591017d
Naxdy <xnaxdy@hydra-development.net>
2024-10-16 15:43:24 +0200

    chore: update `nixpkgs` to `24.05` (#52)

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A minimal NixOS configuration for the RK3588/RK3588S based SBCs";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     nixos-generators = {
