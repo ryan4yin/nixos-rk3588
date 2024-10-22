9f45863d30fe2368038d570f5d22ef9466568d2f
Naxdy <naxdy@naxdy.org>
2024-10-22 12:34:54 +0200

    feat: use mainline mesa

--- modules/boards/base.nix
+++ modules/boards/base.nix
@@ -1,7 +1,4 @@
 { lib
-, pkgs
-, config
-, options
 , ...
 }: {
   config = {
@@ -61,25 +58,6 @@
     };
 
     hardware = {
-      # driver & firmware for Mali-G610 GPU
-      # it works on all rk2588/rk3588s based SBCs.
-      graphics.package =
-        (
-          (pkgs.mesa.override {
-            galliumDrivers = [ "panfrost" "swrast" ];
-            vulkanDrivers = [ "swrast" ];
-          }).overrideAttrs (_: {
-            pname = "mesa-panfork";
-            version = "23.0.0-panfork";
-            src = pkgs.fetchFromGitLab {
-              owner = "panfork";
-              repo = "mesa";
-              rev = "120202c675749c5ef81ae4c8cdc30019b4de08f4"; # branch: csf
-              hash = "sha256-4eZHMiYS+sRDHNBtLZTA8ELZnLns7yT3USU5YQswxQ0=";
-            };
-          })
-        ).drivers;
-
       enableRedistributableFirmware = lib.mkForce true;
     };
   };
